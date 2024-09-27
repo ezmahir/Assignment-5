047cabb0b4bf2f8333e4d4c69a49c714be6372ef
ProgrammingHero1 <mahirruphy@gmail.com>
2024-09-27 21:30:17 +0600

    Added 3 cards in main section

---
+++ JS Files/common.js
@@ -0,0 +1,9 @@
+document.getElementById('go-to-blog')
+    .addEventListener('click', function() {
+        window.location.href = '/blog.html';
+    });
+
+document.getElementById('go-to-home')
+    .addEventListener('click', function() {
+        window.location.href = '/index.html';
+    });
