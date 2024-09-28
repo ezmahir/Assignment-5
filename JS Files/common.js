580809b3977d8a26d9bbe748dd024b233e3abe70
ProgrammingHero1 <mahirruphy@gmail.com>
2024-09-28 22:30:35 +0600

    made the History section

--- JS Files/common.js
+++ JS Files/common.js
@@ -23,7 +23,16 @@ document.getElementById('donate-quota').addEventListener('click', function() {
     document.getElementById('quota-coins').innerText = new_bal;
     deductFromWallet('input-quota');
     my_modal_1.showModal();
-})
+    const time = new Date();
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="w-4/5 mx-auto my-5 p-8 rounded-lg border-2 border-gray-300 space-y-5">
+            <p id="donationDetails" class="font-bold text-xl">${donate} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
+            <p id="donationTimigs" class="font-light text-base">Date : ${time}</p>
+        </div>
+    `
+    document.getElementById('history-page').appendChild(div);
+});
 
 document.getElementById('donate-feni').addEventListener('click', function() {
     const cur_bal = parseFloat(document.getElementById('feni-coins').innerText);
@@ -45,7 +54,16 @@ document.getElementById('donate-feni').addEventListener('click', function() {
     document.getElementById('feni-coins').innerText = new_bal;
     deductFromWallet('input-feni');
     my_modal_1.showModal();
-})
+    const time = new Date();
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="w-4/5 mx-auto my-5 p-8 rounded-lg border-2 border-gray-300 space-y-5">
+            <p id="donationDetails" class="font-bold text-xl">${donate} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+            <p id="donationTimigs" class="font-light text-base">Date : ${time}</p>
+        </div>
+    `
+    document.getElementById('history-page').appendChild(div);
+});
 
 document.getElementById('donate-flood').addEventListener('click', function() {
     const cur_bal = parseFloat(document.getElementById('flood-coins').innerText);
@@ -67,4 +85,23 @@ document.getElementById('donate-flood').addEventListener('click', function() {
     document.getElementById('flood-coins').innerText = new_bal;
     deductFromWallet('input-flood');
     my_modal_1.showModal();
-})
+    const time = new Date();
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="w-4/5 mx-auto my-5 p-8 rounded-lg border-2 border-gray-300 space-y-5">
+            <p id="donationDetails" class="font-bold text-xl">${donate} Taka is Donated for Flood Relief in Noakhali,Bangladesh</p>
+            <p id="donationTimigs" class="font-light text-base">Date : ${time}</p>
+        </div>
+    `
+    document.getElementById('history-page').appendChild(div);
+});
+
+document.getElementById('btn-history').addEventListener('click', function() {
+    document.getElementById('history-page').classList.remove('hidden');
+    document.getElementById('donation-page').classList.add('hidden');
+});
+
+document.getElementById('btn-donation').addEventListener('click', function() {
+    document.getElementById('donation-page').classList.remove('hidden');
+    document.getElementById('history-page').classList.add('hidden');
+});
